1f387ec4bc54924607b64797655fa42c96747bc7
simplymichael <simplymichaelorji@gmail.com>
2020-11-17 17:23:43 +0100

    ci(testing): setup testing
    Setup testing with mocha, chai, and run-script-os

---
+++ __tests__/index.test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const { expect } = chai;
+
+describe('Basic test', function() {
+  it('should pass a basic test', function() {
+    expect(true).to.equal(true);
+  });
+});
