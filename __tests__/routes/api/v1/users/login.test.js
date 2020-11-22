7048410c75b2713b1c2aac90a3b8ed5ca7592bf8
simplymichael <simplymichaelorji@gmail.com>
2020-11-22 16:12:47 +0100

    test(user-login tests): add more tests to cover user login scenarios
    Add tests to cover the following scenarios when login in a user: valid email/username but no
    password, valid password but missing login field, non-existent user (username or email) with valid
    password, valid email/username with valid password

--- __tests__/routes/api/v1/users/login.test.js
+++ __tests__/routes/api/v1/users/login.test.js
@@ -14,25 +14,48 @@ chai.use(chaiHttp);
 describe(`User Login: POST ${loginRoute}`, async () => {
   let clonedUser = { ...userData };
 
-  it('should return a 400 status code if login field is missing', (done) => {
-    let clonedUser = { ...userData };
+  beforeEach(async () => {
+    server.listen(apiPort); // Get the server running and listening on port
+    const signupRoute = `${apiUrl}/users`;
 
+    // First create a user
+    //
+    // For some reason,
+    // chai.request(server).post(signupRoute)
+    // does not play well in creating the user,
+    // and then logging-in afterwards.
+    // So, we use node-fetch instead.
+    await fetch(`http://localhost:${apiPort}${signupRoute}`, {
+      method: 'post',
+      body: JSON.stringify(userData),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  });
+
+  it('should return a 400 status code if login field is missing', (done) => {
     chai.request(server)
       .post(loginRoute)
-      .send(clonedUser)
+      .send({ password: userData.password })
       .end((err, res) => {
         res.should.have.status(400);
         done();
     });
   });
 
-  it('should return a 400 status code if password is missing', (done) => {
-    let clonedUser = { ...userData };
-    delete clonedUser.password;
-
+  it('should return a 400 status code if email is valid and password is missing', (done) => {
     chai.request(server)
       .post(loginRoute)
-      .send(clonedUser)
+      .send({ login: clonedUser.email })
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+    });
+  });
+
+  it('should return a 400 status code if username is valid and password is missing', (done) => {
+    chai.request(server)
+      .post(loginRoute)
+      .send({ login: clonedUser.username })
       .end((err, res) => {
         res.should.have.status(400);
         done();
@@ -120,61 +143,75 @@ describe(`User Login: POST ${loginRoute}`, async () => {
     });
   });
 
-  describe('Successful Login', () => {
-    beforeEach(async () => {
-      let clonedUser = { ...userData };
-      const signupRoute = `${apiUrl}/users`;
-      // 1. First create a user
-      //
-      // For some reason,
-      // chai.request(server).post(signupRoute)
-      // does not play well in creating the user,
-      // and then logging-in afterwards.
-      // So, we use node-fetch instead.
-      //
-      // Also, placing this code inside the it() call causes issues, too.
-      // Therefore, I had to wrap both - this code and it() - inside describe(),
-      // and then wrap this code inside the beforeEach() call
-      server.listen(apiPort); // Get the server running and listening on port
-      await fetch(`http://localhost:${apiPort}${signupRoute}`, {
-        method: 'post',
-        body: JSON.stringify(clonedUser),
-        headers: { 'Content-Type': 'application/json' },
-      });
+  it('should return a 404 status code if login field is not an existent email or username', (done) => {
+    chai.request(server)
+      .post(loginRoute)
+      .send({ login: 'somefunnyemail@yahoo.com', password: clonedUser.password })
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
     });
+  });
 
-    it('should authenticate user with valid email and password', (done) => {
-      let clonedUser = { ...userData };
+  it('should authenticate user with valid email and password', (done) => {
+    chai.request(server)
+      .post(loginRoute)
+      .send({ login: userData.email, password: userData.password })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('data');
+        res.body.data.should.have.property('user');
+        res.body.data.should.have.property('authorization');
 
-      // 2. Then log them in
-      chai.request(server)
-        .post(loginRoute)
-        .send({ login: clonedUser.email, password: clonedUser.password })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          res.body.should.have.property('data');
-          res.body.data.should.have.property('user');
-          res.body.data.should.have.property('authorization');
-
-          for(let [key, value] of Object.entries(clonedUser)) {
-            if(key === 'password' || key === 'confirmPassword') {
-              continue;
-            }
-
-            res.body.data.user.should.have.property(key);
-            res.body.data.user[key].should.equal(value);
+        for(let [key, value] of Object.entries(clonedUser)) {
+          if(key === 'password' || key === 'confirmPassword') {
+            continue;
           }
 
-          res.body.data.authorization.should.have.property('token');
-          res.body.data.authorization.token.should.be.a('string');
-          res.body.data.authorization.token.should.satisfy(
-            msg => msg.startsWith('Bearer '));
-          res.body.data.authorization.should.have.property('expiresIn');
-          res.body.data.authorization.expiresIn.should.be.a('string').and.satisfy(
-            msg => msg === eval(env.AUTH_TOKEN_EXPIRY) + 's');
-          done();
-      });
+          res.body.data.user.should.have.property(key);
+          res.body.data.user[key].should.equal(value);
+        }
+
+        res.body.data.authorization.should.have.property('token');
+        res.body.data.authorization.token.should.be.a('string');
+        res.body.data.authorization.token.should.satisfy(
+          msg => msg.startsWith('Bearer '));
+        res.body.data.authorization.should.have.property('expiresIn');
+        res.body.data.authorization.expiresIn.should.be.a('string').and.satisfy(
+          msg => msg === eval(env.AUTH_TOKEN_EXPIRY) + 's');
+        done();
+    });
+  });
+
+  it('should authenticate user with valid username and password', (done) => {
+    chai.request(server)
+      .post(loginRoute)
+      .send({ login: userData.username, password: userData.password })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('data');
+        res.body.data.should.have.property('user');
+        res.body.data.should.have.property('authorization');
+
+        for(let [key, value] of Object.entries(clonedUser)) {
+          if(key === 'password' || key === 'confirmPassword') {
+            continue;
+          }
+
+          res.body.data.user.should.have.property(key);
+          res.body.data.user[key].should.equal(value);
+        }
+
+        res.body.data.authorization.should.have.property('token');
+        res.body.data.authorization.token.should.be.a('string');
+        res.body.data.authorization.token.should.satisfy(
+          msg => msg.startsWith('Bearer '));
+        res.body.data.authorization.should.have.property('expiresIn');
+        res.body.data.authorization.expiresIn.should.be.a('string').and.satisfy(
+          msg => msg === eval(env.AUTH_TOKEN_EXPIRY) + 's');
+        done();
     });
   });
 });
