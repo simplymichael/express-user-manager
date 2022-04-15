2cd3c214964fe7d0a27c588f6794e78f89789ead
simplymichael <simplymichaelorji@gmail.com>
2022-04-15 20:17:09 +0100

    fix(password validator): fix bug in password validation routine
    When password validation data is set only via UserManager.config() call and not via environment
    variables, the password validator does not see the data and returns null for the password
    configuration object, and undefined for the member values of the object. When password validation is
    set only via environment variables, the password validator also fails to pick up the values, instead
    falling back to the default values. This commit fixes both issues.
    
    "fix #2"

--- src/middlewares/validators/password-validator.js
+++ src/middlewares/validators/password-validator.js
@@ -1,35 +1,9 @@
 const { body } = require('express-validator');
 const { env, appModule } = require('../_utils');
-const passwordConfig = appModule.get('password') || {};
-const disallowed = passwordConfig.disallowed;
-const minLength = passwordConfig.minLength;
-const maxLength = passwordConfig.maxLength;
-
-const [ MIN ] = [ minLength || env.PASSWORD_MIN_LENGTH ]
-  .map(parseInt)
-  .map(val => typeof val === 'number' ? val : 6);
-
-const [ MAX ] = [ maxLength || env.PASSWORD_MAX_LENGTH ]
-  .map(parseInt)
-  .map(val => typeof val === 'number' ? val : 20);
-
-let disallowedList = disallowed;
-
-if(typeof disallowedList === 'string') {
-  disallowedList = disallowedList.trim();
-}
-
-if(!disallowedList || disallowedList.length === 0) {
-  disallowedList = env.DISALLOWED_PASSWORDS;
-}
-
-const DISALLOWED_LIST = Array.isArray(disallowedList)
-  ? disallowedList
-  : typeof disallowedList === 'string'
-    ? disallowedList.split(',').map(str => str.trim())
-    : [];
 
 module.exports = () => {
+  const [MIN, MAX, DISALLOWED_LIST ] = getPasswordValidationData();
+
   return [
     body('password').trim()
       .isLength({
@@ -43,3 +17,41 @@ module.exports = () => {
       .escape()
   ];
 };
+
+
+// Wrap the call to appModule.get('password') inside a function
+// so that by the time we call the function, the appModule will be available and fully initialized.
+// Placing the call outside leads to the call being made before
+// the appModule have fully initialized and registered its config properties.
+function getPasswordValidationData() {
+  const passwordConfig = appModule.get('password') || {};
+  const disallowed = passwordConfig.disallowed;
+  const minLength = passwordConfig.minLength;
+  const maxLength = passwordConfig.maxLength;
+
+  const [ MIN ] = [ minLength || env.PASSWORD_MIN_LENGTH ]
+    .map(parseInt)
+    .map(val => Number.isNaN(val) ? 6 : val);
+
+  const [ MAX ] = [ maxLength || env.PASSWORD_MAX_LENGTH ]
+    .map(parseInt)
+    .map(val => Number.isNaN(val) ? 20 : val);
+
+  let disallowedList = disallowed;
+
+  if(typeof disallowedList === 'string') {
+    disallowedList = disallowedList.trim();
+  }
+
+  if(!disallowedList || disallowedList.length === 0) {
+    disallowedList = env.DISALLOWED_PASSWORDS;
+  }
+
+  const DISALLOWED_LIST = Array.isArray(disallowedList)
+    ? disallowedList
+    : typeof disallowedList === 'string'
+      ? disallowedList.split(',').map(str => str.trim())
+      : [];
+
+  return [MIN, MAX, DISALLOWED_LIST];
+}
